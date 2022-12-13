55c795376f8a05e6f5de77d383f812cf42fda02b
nielsvelst <nielsvelst@hotmail.com>
2022-12-13 23:02:51 +0100

    huiswerk les 3 af versie 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Integer[] numeric = {1,2,3,4,5,6,7,8,9,0};
+        String[] alphabetic = {"een","twee","drie","vier","vijf","zes","zeven","acht","negen","nul"};
+        Transelator translator = new Transelator(alphabetic,numeric);
+        boolean play = true;
+        String ongeldig = "ongeldige invoer";
+        Scanner scanner = new Scanner(System.in);
+        while(play){
+            System.out.println("Type 'x' om te stoppen \nType 'v' om te vertalen");
+            String input = scanner.nextLine();
+            if(input.equals("x")){
+                play = false;
+            } else if (input.equals("v")) {
+                System.out.println("Type een cijfer in van 0 t/m 9");
+                int number = scanner.nextInt();
+                scanner.nextLine();
+                // waarom moet dit worden toegevoegd?
+                if(number < 10){
+                    String result = translator.translate(number);
+                    System.out.println("De vertaling van " + number + " is " + result);
+                } else {
+                    System.out.println("ongeldig");
+                }
+            } else {
+                System.out.println("ongeldig");
+            }
+        }
+
+        System.out.println("programma afgeslotenx");
     }
 }
---
+++ src/Transelator.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Transelator {
+    Map<Integer,String> numericAlpha = new HashMap<>();
+    public Transelator(String[] alphabetic, Integer[] numeric) {
+        for (int i = 0; i < alphabetic.length; i++) {
+            numericAlpha.put(numeric[i],alphabetic[i]);
+        }
+    }
+
+    public String translate(Integer number){
+        return numericAlpha.get(number);
+    }
+// waarom heeft deze methode geen static?
+}
